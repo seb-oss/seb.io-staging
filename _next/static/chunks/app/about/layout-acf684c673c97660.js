d8d109b7fb251fd75eb9ebf78c30dbbe0b1a6af1
Continuous Integration <username@users.noreply.github.com>
2024-02-12 14:03:07 +0000

    Continuous Integration Deployment 9c1516bee8e72c229a002b1bf173114b01a0b459}

---
+++ _next/static/chunks/app/about/layout-acf684c673c97660.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[841,605,686,989,431,978,429,977,131,124,692],{3521:function(){}},function(n){n.O(0,[971,938,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
