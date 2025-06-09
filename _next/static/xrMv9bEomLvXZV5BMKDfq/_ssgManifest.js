18441793e19a01e72ad6ecee1a2744a40ca28edf
Continuous Integration <username@users.noreply.github.com>
2025-06-09 07:29:16 +0000

    Continuous Integration Deployment 1e61bfd8c44bd3618f4ff48c0b25750f4b138c5b}

---
+++ _next/static/xrMv9bEomLvXZV5BMKDfq/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcomponent\u002F[slug]","\u002Fcomponent\u002F[slug]\u002Faccessibility","\u002Fcomponent\u002F[slug]\u002Fux-text"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
